cc8ca68204355150239d5be37feca8d280506844
ilya2U <102806031+ilya2U@users.noreply.github.com>
2024-09-09 23:09:26 +0300

    инпут трясется 2

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './DeleteButton.css';
 
-const DeleteButton = ({deleteAllTodos, onButtonClick}) => {
+const DeleteButton = ({ deleteAllTodos, onButtonClick }) => {
   const curTodos = JSON.parse(localStorage.getItem('todos'));
+
   return (
     <div>
-        <img
-          src="/free-icon-font-trash-3917378.svg"
-          alt="Delete"
-          className="icon"
-          onClick={() => {
-            if(curTodos.length === 0){
-              onButtonClick()
-            }
+      <img
+        src="/free-icon-font-trash-3917378.svg"
+        alt="Delete"
+        className="icon"
+        onClick={() => {
+          if (curTodos.length < 1) {
+            onButtonClick();
             deleteAllTodos();
-           
-          }}
-        />
+          } else {
+            deleteAllTodos();
+          }
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DeleteButton
+export default DeleteButton;
